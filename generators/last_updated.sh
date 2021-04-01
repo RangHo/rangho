8c316d4b3b9478a78a8e01a47f11042e1c610cda
RangHo Lee <hello@rangho.me>
2021-04-01 14:10:38 +0900

    Use proper timezone when determining last updated time

--- generators/last_updated.sh
+++ generators/last_updated.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-badge_content="<img src=\"https://img.shields.io/badge/Last%20updated-$(date +'%m/%d/%Y')-brightgreen?style=flat-square\" alt=\"Last Updated Icon\" />"
+badge_content="<img src=\"https://img.shields.io/badge/Last%20updated-$(env TZ='Asia/Seoul' date +'%m/%d/%Y')-brightgreen?style=flat-square\" alt=\"Last Updated Icon\" />"
 sed -i "s#<!-- last_updated -->#$badge_content#" README.md
