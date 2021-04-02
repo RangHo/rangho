09d35274e012cf7e9c52119a441ce872f1937444
RangHo Lee <hello@rangho.me>
2021-04-02 16:05:06 +0900

    Add contact information

--- generators/last_updated.sh
+++ generators/last_updated.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-badge_content="<img src=\"https://img.shields.io/badge/Last%20updated-$(env TZ='Asia/Seoul' date +'%m/%d/%Y')-brightgreen?style=flat-square\" alt=\"Last Updated Icon\" />"
+badge_content="<a href=\"https://github.com/RangHo/rangho/actions\"><img src=\"https://img.shields.io/badge/Last%20updated-$(env TZ='Asia/Seoul' date +'%D %I:%M %p')-brightgreen?style=flat-square\" alt=\"Last Updated Icon\" /></a>"
 sed -i "s#<!-- last_updated -->#$badge_content#" README.md
